c45d5eed27e598fa26ec3fe787bd6c1c00947f7b
gogomos <abououss2000@gmail.com>
2024-04-19 09:48:52 +0100

    last

--- scripts/magasine-details.js
+++ scripts/magasine-details.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     var modal = document.getElementById("addModalBg");
     var modalBtn = document.querySelector(".btn-ajou");
     var updateModal = document.getElementById("updateModalBg");
+    var deleteModal = document.getElementById("deleteModalBg");
     var updateModalBtn = document.querySelector(".add");
-  
+    var deleteModalBtn = document.querySelector(".actionMD .delete");
+    var closeDeleteBtn = document.getElementById("customCloseBtnDel")
     var closeAddBtn = document.getElementById("customCloseBtn");
     var closeUpdateBtn = document.getElementById("customCloseBtnUp");
     modalBtn.addEventListener("click", function () {
@@ -12,17 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
     updateModalBtn.addEventListener("click", function () {
       updateModal.style.display = "flex";
     });
-  
+    deleteModalBtn.addEventListener("click", function () {
+      deleteModal.style.display = "flex";
+    });
     closeAddBtn.addEventListener("click", function () {
       modal.style.display = "none";
     });
     closeUpdateBtn.addEventListener("click", function () {
       updateModal.style.display = "none";
     });
-  
+    closeDeleteBtn.addEventListener("click", function () {
+      deleteModal.style.display = "none";
+    });
+    
     window.addEventListener("click", function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
+      } else if (event.target == updateModal) {
+        updateModal.style.display = "none";
+      } else if (event.target == deleteModal) {
+        deleteModal.style.display = "none";
       }
     });
 
